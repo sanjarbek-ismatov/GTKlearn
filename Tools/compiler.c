7e4fff4c1e13dc6416e186f0d6990ffd977c97d3
Sanjarbek <ismatovsanjarbek@yandex.ru>
2025-01-21 18:11:03 +0500

    GTK4 compiler in C is ready

--- Tools/compiler.c
+++ Tools/compiler.c
@@ -1,7 +1,8 @@
-// Abandoned for now, I will come back to this later. 
 #include<stdio.h>
 #include<stdlib.h>
 int main(int argc, char **argv){
-    system("gcc %s -o %s `pkg-config --cflags gtk4 --libs gtk4`", argv[1], argv[2]);
+    char command[256];
+    sprintf(command, "eval gcc %s -o %s `pkg-config --cflags --libs gtk4`", argv[1], argv[2]);
+    system(command);
     return 0;
 }
